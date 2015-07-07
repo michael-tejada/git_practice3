7ae6cfa9f9fe1e6b5388239fc66dfc4ae5b8574f
miketejada <mtejada@alumni.stanford.edu>
2015-07-07 14:02:09 +0000

    added weather

--- git_practice.rb
+++ git_practice.rb
@@ -1,4 +1,5 @@
 puts "What's your name?"
 name = gets.chomp
 age = gets.chomp
-puts "Your name is #{name}. You are #{age} years old."
+weather = gets.chomp
+puts "Your name is #{name}. You are #{age} years old. Today is #{weather} :()"
